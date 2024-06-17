01644b073e4492aa97d2b2397354e61478bc8fc5
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-06-17 12:05:04 +0000

    created basic index page

--- interface/app/controllers/slides_controller.rb
+++ interface/app/controllers/slides_controller.rb
@@ -1,5 +1,11 @@
 class SlidesController < ApplicationController
   def index
-    @slides=[1]
+    json_data=Faraday.get(Faraday.get(
+      "https://content-out.bepress.com/v2/digital.kenyon.edu/query",
+      {parent_key: '5047491'}, 
+      {'Authorization' => 'MPqsb8Pd9+YxLgTIC638nB2h0m7vNjyIPzC009gUru8='}).env.response_headers['location']
+    )
+    objects=JSON.parse(json_data.body, object_class: Slide).results
+    @slides=objects
   end
 end
--- interface/app/models/slide.rb
+++ interface/app/models/slide.rb
@@ -1,2 +1,42 @@
-class Slide < ApplicationRecord
+class Slide < OpenStruct
+
+  def medimg
+    begin
+      medlink=self.getImgLinks(self.download_link)[0]
+    rescue
+      medlink="UNFOUND"
+    ensure 
+      return medlink
+    end
+  end
+
+  def thumbnail
+    begin
+      thmlink=self.getImgLinks(self.download_link)[1]
+    rescue
+      medlink="UNFOUND"
+    ensure
+      return thmlink
+    end
+  end
+
+
+  private
+
+  def getImgLinks(sampleLink)
+    linkComponents=sampleLink.split("/")
+    posGuess=linkComponents[6]
+    unless posGuess.to_i.to_s == posGuess
+      linkComponents.each do |guess|
+        print "First Guess: #{posGuess}, new guess: #{guess}"
+        if guess.to_i.to_s == guess
+          posGuess=guess
+          break
+        end
+      end
+    end
+    newMedLink="https://digital.kenyon.edu/baly/#{posGuess}/preview.jpg"
+    newThmLink="https://digital.kenyon.edu/baly/#{posGuess}/thumbnail.jpg"
+    return [newMedLink,newThmLink]
+  end
 end
