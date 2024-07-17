aa70c4806ddb13b9a14cd8504c69ec44cc5947ad
Braeden <singleton1@kenyon.edu>
2024-07-17 14:43:01 -0400

    finished keyword tool

--- samplePages/assets/keywords.js
+++ samplePages/assets/keywords.js
@@ -1,7 +1,22 @@
 var activeMap=0
 
-function readInput() {
-    const text=document.getElementById("text-input").value
+function logError(divid,message){
+    var alert=document.getElementById(divid);
+    alert.innerHTML=message;
+    alert.style="display:block";
+}
+function clearErrors() {
+    errboxes=document.querySelectorAll(".alert");
+    errboxes.forEach((box)=>{
+        box.innerHTML="";
+        box.style="display:hidden";
+    })
+}
+
+function readInput(text=0) {
+    if (text==0){
+        text=document.getElementById("text-input").value
+    }
     if (text.length < 3) {
         return 0;
     }
@@ -33,6 +48,14 @@ function readInput() {
     displayAll()
 }
 
+function pasteInput() {
+    navigator.clipboard.readText().then(function(postitiveresult){
+        readInput(postitiveresult)
+        console.log("worked")
+    },function(negativeResult){
+        logError("err-bar-bad","Clipboard access denied, try again or use textbox")
+    })
+}
 function displayAll() {
     outputbox=document.getElementById("output-container")
     activeMap.forEach((value,key)=>{
@@ -40,10 +63,7 @@ function displayAll() {
             "<span class='keyword-item' style='font-size: "+
             (value*3+70).toString()+"%;'>"+
             key+
-            " </span> "+
-            "<span class='spacer' style='padding:"+
-            (value+5)+
-            "px;'></span>"
+            " </span> "
         )
     })
 }
@@ -54,9 +74,9 @@ function displaySuggestions(input){
         var fillHTML=""
         for (let i=0;i<(words.length-1);i++){
             var item=words[i]
-            fillHTML+= "<p class='suggestion'>"+ item + "<span class='frequency'>("+activeMap.get(item) +")</p><hr class='suggestion-div'>"
+            fillHTML+= "<p class='suggestion rounded' onclick='addToList(this)'>"+ item + "<span class='frequency'>("+activeMap.get(item) +")</p><hr class='suggestion-div'>"
         }
-        fillHTML+="<p class='suggestion'>"+ words.at(-1) + "<span class='frequency'>("+activeMap.get(words.at(-1)) +")</p>"
+        fillHTML+="<p class='suggestion rounded' onclick='addToList(this)'>"+ words.at(-1) + "<span class='frequency'>("+activeMap.get(words.at(-1)) +")</p>"
     }
     else {
         fillHTML="<p class='suggestion'> No Results </p>"
@@ -90,5 +110,61 @@ searchbox.addEventListener("keyup", function(){
     }
     else {
         document.getElementById("suggestions").style="display:none;"
+    }
+})
+searchbox.addEventListener("click",function(){
+    if (searchbox.value.length == 0){
+        document.getElementById("suggestions").style="display:none;"
+    }
+})
+
+const wordlist=document.getElementById("list-items")
+function addToList(pElement){
+    removeOutline();
+    var content=pElement.innerHTML;
+    var word=content.slice(0,content.indexOf("<"));
+    if (wordlist.innerHTML.length < 1){
+        wordlist.innerHTML="<span class='list-item' onclick='deleteListElement(this)'>"+word+"</span>"
+    }
+    else {
+        wordlist.innerHTML+="<span class='list-item' onclick='deleteListElement(this)'>, "+word+"</span>"
+    }
+}
+function removeOutline(){
+    if (Array.from(wordlist.classList).includes("outline-blue")){
+        wordlist.classList.remove("outline-blue")
+    }
+}
+function clearList(){
+    removeOutline()
+    wordlist.innerHTML=""
+}
+function deleteListElement(element) {
+    removeOutline()
+    var children=Array.from(wordlist.children)
+    element.remove()
+    if (children.indexOf(element) == 0 && children.length > 1){
+        var nextelement=children.at(1)
+        nextelement.innerHTML=nextelement.innerHTML.slice(2)
+    }
+}
+function copyList(){
+    var children=Array.from(wordlist.children);
+    listString=new String;
+    children.forEach((item)=>{
+        if (item.innerHTML.length > 0) {
+            listString+=item.innerHTML;
+        }
+    })
+    wordlist.classList.add("outline-blue")
+    navigator.clipboard.writeText(listString)
+}
+document.getElementById("key-search").addEventListener("keypress", function(e){
+    if (e.key ==="Enter") {
+        var suggList=document.getElementById("suggestions").children;
+        pElement=Array.from(suggList).at(0);
+        addToList(pElement);
+        document.getElementById("key-search").value="";
+        document.getElementById("suggestions").style="display:none;";
     }
 })
