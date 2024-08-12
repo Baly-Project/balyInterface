dc096bb7f59fc15937ef41255026799e93b27b9e
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-08-12 17:48:40 +0000

    added orientation controller

--- interface/app/javascript/controllers/index.js
+++ interface/app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import OrientController from "./orient_controller"
+application.register("orient", OrientController)
