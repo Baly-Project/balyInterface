5da8e73b202e57cc1209ef708b7a6aaa668333e7
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-08-17 05:16:27 +0000

    before adding keywords

---
+++ interface/app/models/custom_pattern.rb
@@ -0,0 +1,8 @@
+class CustomPattern
+  #this is a user-input class that forces screening of patterns before they are displayed
+ 
+  KeywordSubsets={# A hash representing keywords that are completely included within others.
+    # This should be reserved for logical subsets and avoid coincidences.
+    # To update, generate 
+    
+end
