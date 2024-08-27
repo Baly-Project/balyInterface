8978a250a58394b3e97043562af95148df8239a9
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-08-27 14:06:02 +0000

    styled keyword pages

--- interface/app/controllers/keywords_controller.rb
+++ interface/app/controllers/keywords_controller.rb
@@ -1,11 +1,22 @@
 class KeywordsController < ApplicationController
   def index
-    @keywords=Keyword.all.sort_by {|kw| -kw.previews.length}
+    @keywords=Keyword.all
     @periods=Keyword.first.getPeriods
+    @periodObjects=assembleHash(@periods)
   end
 
   def show
     id=params[:id]
     @keyword=Keyword.find(id)
   end
+
+  def assembleHash(periods)
+    titles=Array.new
+    periods.each do |key,list|
+      titles+=list
+    end
+    words=Keyword.where(:title => titles).pluck(:title,:id)
+    return words.to_h
+  end
 end
+
