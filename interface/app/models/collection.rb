8ad6c1874cab43347ee169fb5bf7ec1a78db5e43
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-09-25 05:38:56 +0000

    resolved all-portrait case

--- interface/app/models/collection.rb
+++ interface/app/models/collection.rb
@@ -2,13 +2,19 @@ class Collection < ApplicationRecord
   has_many :previews
 
   def getRandomImage
-    unfound=true
-    (self.previews.size*3).times do |i|
-      preview=self.previews.sample
-      if preview.orientation == "L"
-        return preview
-        unfound=falsex
+    begin
+      sample=self.previews.find_by! orientation:"L"
+      unfound=true
+      (self.previews.size*3).times do |i|
+        preview=self.previews.sample
+        if preview.orientation == "L"
+          return preview
+          unfound=false
+        end
       end
+      return sample
+    rescue
+      return self.previews.sample
     end
   end
 end
