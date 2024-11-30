e9f8295b542c764db6b881ccfe47a4285aa27546
Braeden <singleton1@kenyon.edu>
2024-11-30 15:02:55 -0500

    deleted accidental model

--- interface/app/controllers/bigmap_controller.rb
+++
@@ -1,2 +0,0 @@
-class BigmapController < ApplicationController
-end
--- interface/test/controllers/bigmap_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class BigmapControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
