d48706413e972d3451ae0b0c133dc405a34db49b
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-12-29 02:36:06 +0000

    working autofiller

--- interface/app/javascript/controllers/autofill_controller.js
+++ interface/app/javascript/controllers/autofill_controller.js
@@ -37,12 +37,12 @@ export default class extends Controller {
 
   // Search methods
   autofill(query){
-    if(!query.includes(this.laststring)) {
+    if(!query.includes(this.laststring) || !this.keys) {
       this.setKeys();
     };
     var results = [];
     this.keys.forEach(element => {
-      if (element.includes(query)){
+      if (element.toLowerCase().includes(query.toLowerCase())){
         results.push(element)
       }
     });
@@ -55,8 +55,8 @@ export default class extends Controller {
 
   prepareElements(array,searchstring){
     array.sort(function(a,b){
-      var anum=a.indexOf(searchstring);
-      var bnum=b.indexOf(searchstring);
+      var anum=a.toLowerCase().indexOf(searchstring.toLowerCase());
+      var bnum=b.toLowerCase().indexOf(searchstring.toLowerCase());
       if (anum < bnum){
         return -1;
       } else if (anum > bnum){
@@ -67,8 +67,11 @@ export default class extends Controller {
     var topN = array.slice(0,this.maxresultsValue+1)
     var elements =  ''
     topN.forEach(result => {
-      var itemstring = "<a class='autofill dropdown-item' href='"+searchdata[result]+"'> "+result+"</a> <hr class='thinline'>";
-      elements+=itemstring;
+      searchdata[result].forEach(link => {
+        var type = this.getType(link)
+        var itemstring = "<a class='autofill dropdown-item' href='"+link+"'> "+result+" <span class='item-type'> "+type+"</span> </a> <hr class='thinline'>";
+        elements+=itemstring;
+      })
     })
     this.fillboxTarget.innerHTML = elements
   }
@@ -84,5 +87,18 @@ export default class extends Controller {
     this.fillboxTarget.style.visibility = 'visible';
     this.fillboxTarget.style.width = " "+targetwidth+"px";
     console.log(targetwidth);
+  }
+
+  getType(link) {
+    const linkIdentifiers = ['collections','keywords','locations','cities','regions','countries','stamps','years'];
+    const types = ['Collection','Keyword','Location','City','Region','Country','Stamp','Year'];
+    var index=0;
+    while(index < linkIdentifiers.length){
+      if(link.includes(linkIdentifiers[index])){
+        return types[index];
+      }
+      index++;
+    }
+    return 'Typing Error';
   }
 }
