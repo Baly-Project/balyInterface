7d12c6246e40797623e48c669e17a8c865e0fb08
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2025-01-02 08:48:21 +0000

    moved getRandomImage into ApplicationRecord

--- interface/app/models/application_record.rb
+++ interface/app/models/application_record.rb
@@ -1,3 +1,20 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def getRandomImage
+    begin
+      sample=self.previews.find_by! orientation:"L"
+      unfound=true
+      (self.previews.size*3).times do |i|
+        preview=self.previews.sample
+        if preview.orientation == "L"
+          return preview
+          unfound=false
+        end
+      end
+      return sample
+    rescue
+      return self.previews.sample
+    end
+  end
 end
--- interface/app/models/collection.rb
+++ interface/app/models/collection.rb
@@ -1,23 +1,6 @@
 class Collection < ApplicationRecord
   has_many :previews
 
-  def getRandomImage
-    begin
-      sample=self.previews.find_by! orientation:"L"
-      unfound=true
-      (self.previews.size*3).times do |i|
-        preview=self.previews.sample
-        if preview.orientation == "L"
-          return preview
-          unfound=false
-        end
-      end
-      return sample
-    rescue
-      return self.previews.sample
-    end
-  end
-
   def generateSearchEntry
     link = "/collections/#{self.alph_value}"
     title = self.title
