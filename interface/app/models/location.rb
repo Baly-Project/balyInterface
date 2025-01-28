7fff63ea35787f6c39fdd11425f0770a01984d5f
Braeden <singleton1@kenyon.edu>
2025-01-28 16:41:16 -0500

    formatted code

--- interface/app/models/location.rb
+++ interface/app/models/location.rb
@@ -8,15 +8,15 @@ class Location < ApplicationRecord
   validates :coordinates, presence: true
 
   def generateMapArrays
-    coords=[prepcoords(self.coordinates)]
-    labels=[self.title]
-    scales=[1.4]
+    coords = [prepcoords(self.coordinates)]
+    labels = [self.title]
+    scales = [1.4]
     self.previews.each do |preview|
       if preview.coordinates.to_s.length > 2
         coords.push prepcoords(preview.coordinates)
-        label="<a class='purple' href='/slides/"
-        label+= preview.sorting_number.to_s 
-        label+= "' data-action='click->back-forth/#setList'> #{preview.title} </a>"
+        label = '<a class="purple" href="/slides/"'
+        label += preview.sorting_number.to_s 
+        label += "' data-action='click->back-forth/#setList'> #{preview.title} </a>"
         labels.push label
         scales.push 1
       end
@@ -24,10 +24,10 @@ class Location < ApplicationRecord
     return {coordinates:coords,labels:labels,scales:scales}
   end
   def prepcoords(coordstring)
-    halves=coordstring.split(",")
-    cord1=halves[0][1..-1].to_f
-    cord2=halves[1][0...-1].to_f
-    arr=[cord1,cord2]
+    halves = coordstring.split(",")
+    cord1 = halves[0][1..-1].to_f
+    cord2 = halves[1][0...-1].to_f
+    arr = [cord1,cord2]
     return arr
   end
 
